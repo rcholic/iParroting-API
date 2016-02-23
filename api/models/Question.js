c38167cdb6ff65aae80927a778c35bad61cbde08
rcholic <ivytony@gmail.com>
2016-02-22 19:34:19 -0500

    comment

--- api/models/Question.js
+++ api/models/Question.js
@@ -1,6 +1,6 @@
 /**
 * Question.js
-*
+* Associated with user, answer, comment, vote, tag, favorite, redFlag
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
